4f4af2902ed9c85fc45838ea7ed3b95f60ce57fb
rmh2805 <rmh2805@g.rit.edu>
2021-07-30 20:48:37 -0400

    Stubbed out some basic functions for characters

--- charData.h
+++ charData.h
@@ -74,4 +74,39 @@ typedef struct character_s {
 
 } character_t;
 
+/**
+ * Generates a null-initialized character
+ * 
+ * @return A null-initialized character struct
+ */
+character_t mkBlankCharacter();
+
+
+/**
+ * Free all allocated data associated with the character
+ * 
+ * @param character The character to free from
+ * @return An exit status (0 on success, <0 on failure)
+ */
+int freeCharacterData(character_t character);
+
+/**
+ * Loads a character from file
+ * 
+ * @param savePath The path of the character's save directory
+ * @param character A return pointer for the character struct
+ * @return An exit status (0 on success, <0 on failure)
+ */
+int loadCharacter(const char * savePath, character_t * character);
+
+
+/**
+ * Writes the provided character's information out to file
+ * 
+ * @param character The character to write to file
+ * @return An exit status (0 on success, <0 on failure)
+ */
+int writeCharacter(character_t character);
+
+
 #endif
