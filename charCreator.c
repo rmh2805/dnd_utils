7b0eed11384a8ecfd5d46039669a1f0373859187
rmh2805 <rmh2805@g.rit.edu>
2021-07-30 23:00:21 -0400

    Got basic prompts down

--- charCreator.c
+++ charCreator.c
@@ -10,19 +10,46 @@
 #include "fs_unix.h"
 #endif
 
+#define getText(ch, buf, nBuf, field, prompt) \
+    printf("%s: ", prompt);\
+    fgets(buf, nBuf, stdin);\
+    strTrim(buf);\
+    ch.field = calloc(strlen(buf) + 1, sizeof(char));\
+    strcpy(ch.field, buf);
+
+#define textOut(ch, field, prompt)\
+    printf("%s: %s\n", prompt, (ch.field == NULL) ? "" : ch.field)
+
+void printCharacterOverview(character_t character) {
+    textOut(character, name, "Name");
+    textOut(character, playerName, "Player name");
+    textOut(character, race, "Race");
+    textOut(character, background, "Background");
+}
+
 int main() {
     char inBuf[256];
     int ret;
 
     character_t character = mkBlankCharacter();
     
+    while (1) {
+        getText(character, inBuf, 256, playerName, "Enter the player's name")
+        getText(character, inBuf, 256, name, "Enter the character's name")
+        getText(character, inBuf, 256, race, "Enter the character's race")
+        getText(character, inBuf, 256, background, "Enter the character's background")
+        
+        printf("\n");
+        printCharacterOverview(character);
+        printf("\nDoes this look right? [Y/n]");
+        
+        fgets(inBuf, 256, stdin);
+        strTrim(inBuf);
+        if(inBuf[0] != 'n' && inBuf[0] != 'N') break;
 
-    // Grab all data required for the 
-    printf("Enter a character name: ");
-    fgets(inBuf, 256, stdin);
-    strTrim(inBuf);
-    character.name = calloc(strlen(inBuf) + 1, sizeof(char));
-    strcpy(character.name, inBuf);
+        printf("\n");
+    }
+    
 
     // Cleanup
     freeCharacterData(character);
